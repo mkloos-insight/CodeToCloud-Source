c7a59513a878f4e776fe8c04e5735270ce3ad933
Kloos <Michael.Kloos@insight.com>
2022-07-19 05:20:26 -0400

    Work on Terraform deployment solution AB#14

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -20,7 +20,7 @@ variable "instance_id" {
 
 resource "azurerm_resource_group" "rg" {
   name      = "fabmedical-rg-${var.instance_id}"
-  location  = "centralus"
+  location  = "eastus"
 }
 
 # Create virtual network
@@ -43,6 +43,7 @@ resource "azurerm_subnet" "snet" {
 resource "azurerm_public_ip" "pip" {
   name                = "fabmedical-pip"
   location            = azurerm_resource_group.rg.location
+  domain_name_label   = "fabmedical-pipdnl"
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Dynamic"
 }
@@ -64,10 +65,22 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+    name                       = "WebApp"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3000"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 # Create network interface
-resource "azurerm_network_interface" "nic" {
+resource "azurerm_network_interface" "nnic" {
   name                = "fabmedical-nic"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -82,12 +95,8 @@ resource "azurerm_network_interface" "nic" {
 
 # Connect the security group to the network interface
 resource "azurerm_network_interface_security_group_association" "nsgass" {
-  network_interface_id      = azurerm_network_interface.nic.id
+  network_interface_id      = azurerm_network_interface.nnic.id
   network_security_group_id = azurerm_network_security_group.nsg.id
-}
-
-data "tls_public_key" "pubkey" {
-  private_key_openssh = file("~/.ssh/id_rsa.pub")
 }
 
 # Create virtual machine
@@ -95,13 +104,12 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name                  = "fabmedical-vm"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.nic.id]
-  size                  = "Standard_B1"
+  network_interface_ids = [azurerm_network_interface.nnic.id]
+  size                  = "Standard_B1s"
 
   os_disk {
     name                 = "fabmedical-osdisk"
     caching              = "ReadWrite"
-    create_option        = "FromImage"
     storage_account_type = "Standard_LRS"
   }
 
@@ -118,6 +126,35 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   admin_ssh_key {
     username   = "azureuser"
-    public_key = tls_public_key.pubkey.public_key_openssh
+    public_key = file("~/.ssh/id_rsa.pub")
   }
+
+  connection {
+    type        = "ssh"
+    user        = "azureuser"
+    private_key = file("~/.ssh/id_rsa")
+    host        = azurerm_public_ip.pip.fqdn
+  }
+
+  provisioner "remote-exec" {
+    scripts = [
+      "temp_vm_init.sh"
+    ]
+  }
+}
+
+data "azurerm_public_ip" "pip" {
+  name                = azurerm_public_ip.pip.name
+  resource_group_name = azurerm_resource_group.rg.name
+  #depends_on          = [time_sleep.wait_1_second]
+}
+output "assigned_username" {
+  value = azurerm_linux_virtual_machine.vm.admin_username
+}
+output "assigned_pip" {
+  #value = azurerm_public_ip.pip.ip_address
+  value = data.azurerm_public_ip.pip.ip_address
+}
+output "assigned_fqdn" {
+  value = azurerm_public_ip.pip.fqdn
 }
