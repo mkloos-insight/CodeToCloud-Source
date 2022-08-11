7d499397de97702675c966ba83e1ecb943c96f9f
Sharon Wang <sharonwang@FabrikamMedical.example.org>
2022-08-11 05:40:03 +0000

    Added Infrastructure as Code file to provision Cloud Infra :cloud:

--- infrastructure/deploy-infrastructure.ps1
+++ infrastructure/deploy-infrastructure.ps1
@@ -1,52 +1,25 @@
-param
-(
-    [string] $studentprefix = "mtk"
-)
-
+$studentprefix = "your abbreviation here"
 $resourcegroupName = "fabmedical-rg-" + $studentprefix
 $cosmosDBName = "fabmedical-cdb-" + $studentprefix
 $webappName = "fabmedical-web-" + $studentprefix
 $planName = "fabmedical-plan-" + $studentprefix
-$location1 = "centralus"
-$location2 = "eastus"
-$appInsights = "fabmedicalai-" + $studentsuffix
+$location1 = "westeurope"
+$location2 = "northeurope"
 
 #First create a group
-$rg = az group create --name $resourcegroupName --location $location1 | ConvertFrom-Json 
+$resultRG = az group create --name $resourcegroupName --location $location1 | ConvertFrom-Json
 
 #Then create a CosmosDB
-az cosmosdb create --name $cosmosDBName `
+$resultCosmos = az cosmosdb create --name $cosmosDBName `
 --resource-group $resourcegroupName `
 --locations regionName=$location1 failoverPriority=0 isZoneRedundant=False `
 --locations regionName=$location2 failoverPriority=1 isZoneRedundant=True `
 --enable-multiple-write-locations `
---kind MongoDB
+--kind MongoDB `
+| ConvertFrom-Json
 
 #Create a Azure App Service Plan
 az appservice plan create --name $planName --resource-group $resourcegroupName --sku S1 --is-linux
 
-az webapp config appsettings set --settings DOCKER_REGISTRY_SERVER_URL="https://ghcr.io" --name $($webappName) --resource-group $($resourcegroupName) 
-az webapp config appsettings set --settings DOCKER_REGISTRY_SERVER_USERNAME="notapplicable" --name $($webappName) --resource-group $($resourcegroupName) 
-az webapp config appsettings set --settings DOCKER_REGISTRY_SERVER_PASSWORD="$($env:CR_PAT)" --name $($webappName) --resource-group $($resourcegroupName) 
-
-
 #Create a Azure Web App with NGINX container
-az webapp create `
---multicontainer-config-file docker-compose.yml `
---multicontainer-config-type COMPOSE `
---name $($webappName) `
---resource-group $($resourcegroupName) `
---plan $($planName)
-
-az webapp config container set `
---docker-registry-server-password $($env:CR_PAT) `
---docker-registry-server-url https://ghcr.io `
---docker-registry-server-user notapplicable `
---multicontainer-config-file docker-compose.yml `
---multicontainer-config-type COMPOSE `
---name $($webappName) `
---resource-group $resourcegroupName 
-
-az extension add --name application-insights
-az monitor app-insights component create --app $appInsights --location $location1 --kind web -g $resourcegroupName --application-type web --retention-time 120
-
+az webapp create --resource-group $resourcegroupName --plan $planName --name $webappName -i nginx
